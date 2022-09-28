9953cf77cafe1fecba9a246abdb7ae2fc883dc3a
patdel0 <zefiru@protonmail.com>
2022-09-28 11:04:54 +0100

    Add `db.js`

--- database/db.js
+++ database/db.js
@@ -8,15 +8,4 @@ const schemaPath = join('database', 'schema.sql')
 const schema = readFileSync(schemaPath, 'utf-8')
 db.exec(schema)
 
-// const createQuery = db.prepare(
-//   /*sql*/
-//   `INSERT name INTO restaurants VALUES (
-//     1, 'alksjdf', 'kjlakjdlkfja', 'laksdjflkajsdflkaj1', '123') ON CONFLICT DO NOTHING`
-// )
-
-// createQuery.run()
-
-// const listQuery = db.prepare(`SELECT * from restaurants`)
-// console.log(listQuery.all())
-
 module.exports = db
