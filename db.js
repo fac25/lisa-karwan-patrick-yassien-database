583fd93b7b6b73ad5e7a71e0e53ca61bcaf38e56
yassienAbdillahi <95880683+yassienAbdillahi@users.noreply.github.com>
2022-09-28 11:09:54 +0100

    delete second db.js file

--- db.js
+++
@@ -1,7 +0,0 @@
-const { readFileSync } = require('node:fs')
-const { join } = require('node:path')
-const Database = require('better-sqlite3')
-
-const db = Database(process.env.DB_FILE)
-
-const schema
