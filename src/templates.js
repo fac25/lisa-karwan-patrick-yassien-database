f57c215ad8c336dd9c201e48bc7d89ca5d21b3d9
patdel0 <zefiru@protonmail.com>
2022-09-29 15:36:23 +0100

    Add accessibility structure `Relates #12`

--- src/templates.js
+++ src/templates.js
@@ -1,4 +1,4 @@
-const { getAllRestaurants } = require("../model/restaurants");
+const { getAllRestaurants } = require('../model/restaurants')
 
 function displayHome(error = {}, values = {}) {
   return /*html*/ `
@@ -10,73 +10,80 @@ function displayHome(error = {}, values = {}) {
           <title>Restaurants</title>
         </head>
         <body>
-          <h1>Restaurants</h1>
-          ${displayForm(error, values)}
-          ${displayRestaurants()}
+          <header>
+            <h1>Finsbo!</h1>
+          </header>
+          <main>
+            ${displayError(error.name)}
+            ${displayForm(values)}
+            <h2>Finsbury Park Restaurants</h2>
+            ${displayRestaurants()}
+          </main>
         </body>
       </html>
-    `;
+    `
 }
 
-function displayForm(error, values) {
+function displayForm(values) {
   return /*html*/ `
     <form method='POST'>
-    ${displayError(error.name)}
       <label for='name'>Name</label>
-      <input type='text' id='name' name='name' value="${handleValue(
+      <input type='text' id='name' name='name' aria-describedBy='name-error' value="${handleValue(
         values.name
-      )}"/>
+      )}">
 
       <label for='description'>Description</label>
       <input type='text' id='description' name='description' value="${handleValue(
         values.description
-      )}"/>
+      )}">
 
       <label for='address'>Address</label>
       <input type='text' id='address' name='address' value="${handleValue(
         values.address
-      )}"/>
+      )}">
      
       <label for='price_range'>Price Range</label>
       <input type='text' id='price_range' name='price_range' value="${handleValue(
         values.price_range
-      )}"/>
+      )}">
 
 
-      <button type='submit'>Submit</button>
+      <button type='submit'>Add Restaurant</button>
     </form>
-  `;
+  `
 }
 
 function displayRestaurants() {
   return /*html */ `<ul>
     ${getAllRestaurants()
       .map((res) => {
-        return /*html */` 
+        return /*html */ ` 
             <li>
             <div>
-                <h2>${res.name}</h2>
-                <p>${res.price_range ? 'Price Range: ' + res.price_range: ''}</p>
+                <h3>${res.name}</h2>
+                <p>${
+                  res.price_range ? 'Price Range: ' + res.price_range : ''
+                }</p>
                 </div>
                 <p>${res.address}</p>
                 <p>${res.description}</p>
             </li>
-        `;
+        `
       })
-      .join("")}
-    </ul>`;
+      .join('')}
+    </ul>`
 }
 
 function handleValue(value) {
-  return value ? sanitize(value) : "";
+  return value ? sanitize(value) : ''
 }
 
 function sanitize(str) {
-  return str.replaceAll("<", "&lt");
+  return str.replaceAll('<', '&lt')
 }
 
 function displayError(error) {
-  return error ? `<span style="color: red">${error}</span>` : "";
+  return error ? `<span id='name-error' style="color: red">${error}</span>` : ''
 }
 
-module.exports = { home: displayHome };
+module.exports = { home: displayHome }
