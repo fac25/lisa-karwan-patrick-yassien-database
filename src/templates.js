77bdabafb8862fb28b2ead24bce53f6f39158703
y <johndoe@example.com>
2022-09-29 14:56:24 +0100

    started basic css
    Relates #11

--- src/templates.js
+++ src/templates.js
@@ -8,10 +8,13 @@ function home(restaurantArr, error = {}, values = {}) {
           <meta charset="utf-8">
           <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
           <title>Restaurants</title>
+          <link rel="stylesheet" type="text/css" href="style.css">
         </head>
         <body>
+        <div class="center width-sm">
           <h1>Restaurants</h1>
           ${form(error, values)}
+        </div>
           ${displayRestaurants()}
           </body>
       </html>
@@ -22,25 +25,25 @@ function form(error, values) {
   return /*html*/ `
     <form method='POST'>
     ${validation(error.name)}
-      <label for='name'>Name</label>
+      <label for='name'>Name:</label>
       <input type='text' id='name' name='name' value="${handleValue(
         values.name
-      )}"/>
+      )}"/><br><br>
 
-      <label for='description'>Description</label>
+      <label for='description'>Description:</label>
       <input type='text' id='description' name='description' value="${handleValue(
         values.description
-      )}"/>
+      )}"/><br><br>
 
-      <label for='address'>Address</label>
+      <label for='address'>Address:</label>
       <input type='text' id='address' name='address' value="${handleValue(
         values.address
-      )}"/>
+      )}"/><br><br>
      
-      <label for='price_range'>Price Range</label>
+      <label for='price_range'>Price Range:</label>
       <input type='text' id='price_range' name='price_range' value="${handleValue(
         values.price_range
-      )}"/>
+      )}"/><br><br>
 
 
       <button type='submit'>Submit</button>
@@ -49,14 +52,15 @@ function form(error, values) {
 }
 
 const displayRestaurants = () => {
-  return /*html */ `<ul>
+  return /*html */ `<ul class="center">
+    <div class="grid">
     ${getAllRestaurants()
       .map((res) => {
         return /*html */ ` 
             <li>
             <div>
                 <h2>${res.name}</h2>
-                <p>Price range${res.price_range}</p>
+                <p>Price range: ${res.price_range}</p>
                 </div>
                 <p>${res.address}</p>
                 <p>${res.description}</p>
@@ -64,7 +68,9 @@ const displayRestaurants = () => {
         `;
       })
       .join("")}
-    </ul>`;
+      </div>
+    </ul>
+  `;
 };
 
 function handleValue(value) {
