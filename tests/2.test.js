e45d49f4296b49a5ba9a9f7d33353451a00415bf
lisahns <lisa.hanes@hotmail.de>
2022-09-30 11:06:38 +0100

    closes #46

--- tests/2.test.js
+++ tests/2.test.js
@@ -2,18 +2,15 @@ const test = require('node:test')
 const assert = require('node:assert')
 const { insertRestaurant } = require('../model/restaurants.js')
 
+const itemWeAreInserting = {
+  name: 'yassiens restaurant',
+  description: 'a place that serves a lot of desserts',
+  address: 'SPACE 4',
+  price_range: 5,
+};
+
 test('add item to database', () => {
-  const insertedRestaurant = insertRestaurant({
-    name: 'yassiens restaurant',
-    description: 'a place that serves a lot of desserts',
-    address: 'SPACE 4',
-    price_range: 5,
-  })
-  const expectedRestaurant = {
-    name: 'yassiens restaurant',
-    description: 'a place that serves a lot of desserts',
-    address: 'SPACE 4',
-    price_range: 5,
-  }
+  const insertedRestaurant = insertRestaurant(itemWeAreInserting);
+  const expectedRestaurant = itemWeAreInserting;
   assert.deepEqual(insertedRestaurant, expectedRestaurant)
 })
